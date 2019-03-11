fc041dffa08b494bba2a10e7d91036c169ecc597
Kalin Chernev <kalin.chernev@gmail.com>
2019-03-11 23:06:48 +0200

    Deployable with amplify publish

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Formik } from "formik";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Deployment works.</p>
+      </div>
+    );
+  }
+}
+
+export default App;

ce0f8bb3de3ca1819507e75daa6ead8044945469
Kalin Chernev <kalin.chernev@gmail.com>
2019-06-09 16:42:04 +0300

    User profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import { Auth } from 'aws-amplify';
 
-const Profile = () => <div>The user profile page</div>;
+class Profile extends Component {
+  state = { user: {}, isLoading: true };
+
+  async componentDidMount() {
+    try {
+      const user = await Auth.currentAuthenticatedUser();
+      this.setState({ user, isLoading: false });
+    } catch (error) {
+      console.error(
+        'There was an issue while fetching information about current user',
+        error
+      );
+    }
+  }
+
+  render() {
+    const { user, isLoading } = this.state;
+
+    console.log(user);
+
+    return (
+      <Fragment>
+        <p>User profile</p>
+        {isLoading ? (
+          <p>Loading ...</p>
+        ) : (
+          <p>{JSON.stringify(user, null, 2)}</p>
+        )}
+      </Fragment>
+    );
+  }
+}
 
 export default Profile;
